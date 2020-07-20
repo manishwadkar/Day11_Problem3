ca9ac8849a20d33cad8e8299e9db507682bac6c1
“manishwadkar” <wadkarmanish@gmail.com>
2020-07-20 12:23:25 +0530

    UC3-Find-Percentage-Doublet-Combination implemented

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -14,8 +14,14 @@ fi
 
 declare -A flipcoin
 singlet=(H T)
+doublet=(HH TT HT TH)
 
 for key in ${singlet[@]}
+do
+        flipcoin[$key]=0
+done
+
+for key in ${doublet[@]}
 do
         flipcoin[$key]=0
 done
@@ -26,6 +32,18 @@ do
         flipcoin[${singlet[$res]}]=$((${flipcoin[${singlet[$res]}]}+1))
 done
 
-echo "Percentage of Heads: " $((${flipcoin[H]}*100/100))
-echo "Percentage of Tails: " $((${flipcoin[T]}*100/100))
+echo "Singlet Combination"
+echo "Percentage of H: " $((${flipcoin[H]}*100/100))
+echo "Percentage of T: " $((${flipcoin[T]}*100/100))
 
+for((i=0;i<100;i++))
+do
+        res1=$((RANDOM%4))
+        flipcoin[${doublet[$res1]}]=$((${flipcoin[${doublet[$res1]}]}+1))
+done
+
+echo "Doublet Combination"
+echo "Percentage of HH" $((${flipcoin[HH]}*100/100))
+echo "Percentage of TT" $((${flipcoin[TT]}*100/100))
+echo "Percentage of HT" $((${flipcoin[HT]}*100/100))
+echo "Percentage of TH" $((${flipcoin[TH]}*100/100))
