b77ac8190462bf94db08e1f8965e0ae8a2851050
“manishwadkar” <wadkarmanish@gmail.com>
2020-07-20 11:55:05 +0530

    UC1-Display-Winner implemented

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,3 +2,12 @@
 
 echo "Welcome to flip Coin Combination"
 
+echo "Flipping coin one time"
+
+result=$(($(($RANDOM%10))%2))
+if [ $result -eq 1 ]
+then
+	echo "Heads Won"
+else
+	echo "Tails Won"
+fi
