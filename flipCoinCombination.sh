05f3e95dab7f1983c651b168416c3d11765ae668
“manishwadkar” <wadkarmanish@gmail.com>
2020-07-20 12:07:06 +0530

    UC2-Find-Percentage-Singlet-Combination implemented

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -11,3 +11,21 @@ then
 else
 	echo "Tails Won"
 fi
+
+declare -A flipcoin
+singlet=(H T)
+
+for key in ${singlet[@]}
+do
+        flipcoin[$key]=0
+done
+
+for((i=0;i<100;i++))
+do
+        res=$((RANDOM%2))
+        flipcoin[${singlet[$res]}]=$((${flipcoin[${singlet[$res]}]}+1))
+done
+
+echo "Percentage of Heads: " $((${flipcoin[H]}*100/100))
+echo "Percentage of Tails: " $((${flipcoin[T]}*100/100))
+
